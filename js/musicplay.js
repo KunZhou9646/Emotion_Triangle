f8d5c3b0a196cd3a8d57c5623f5ca54827ba6fc5
Kun Zhou <kun@Kuns-MacBook-Pro.local>
2022-10-24 15:55:18 -0500

    Emotion_Triangle

---
+++ js/musicplay.js
@@ -0,0 +1,615 @@
+let music1 = document.querySelector('#music1')
+let music2 = document.querySelector('#music2')
+let music3 = document.querySelector('#music3')
+let music4 = document.querySelector('#music4')
+let music5 = document.querySelector('#music5')
+let music6 = document.querySelector('#music6')
+let music7 = document.querySelector('#music7')
+let music8 = document.querySelector('#music8')
+let music9 = document.querySelector('#music9')
+let music10 = document.querySelector('#music10')
+let music11 = document.querySelector('#music11')
+let music12 = document.querySelector('#music12')
+let music13 = document.querySelector('#music13')
+let music14 = document.querySelector('#music14')
+let music15 = document.querySelector('#music15')
+let music16 = document.querySelector('#music16')
+let music17 = document.querySelector('#music17')
+let music18 = document.querySelector('#music18')
+let music19 = document.querySelector('#music19')
+let music20 = document.querySelector('#music20')
+let music21 = document.querySelector('#music21')
+let music22 = document.querySelector('#music22')
+let music23 = document.querySelector('#music23')
+let music24 = document.querySelector('#music24')
+let music25 = document.querySelector('#music25')
+let music26 = document.querySelector('#music26')
+let music27 = document.querySelector('#music27')
+let music28 = document.querySelector('#music28')
+let music29 = document.querySelector('#music29')
+let music30 = document.querySelector('#music30')
+let music31 = document.querySelector('#music31')
+let music32 = document.querySelector('#music32')
+let music33 = document.querySelector('#music33')
+let music34 = document.querySelector('#music34')
+let music35 = document.querySelector('#music35')
+let music36 = document.querySelector('#music36')
+let music37 = document.querySelector('#music37')
+let music38 = document.querySelector('#music38')
+let music39 = document.querySelector('#music39')
+let music40 = document.querySelector('#music40')
+let music41 = document.querySelector('#music41')
+let music42 = document.querySelector('#music42')
+let music43 = document.querySelector('#music43')
+let music44 = document.querySelector('#music44')
+let music45 = document.querySelector('#music45')
+let music46 = document.querySelector('#music46')
+let music47 = document.querySelector('#music47')
+let music48 = document.querySelector('#music48')
+let music49 = document.querySelector('#music49')
+let music50 = document.querySelector('#music50')
+let music51 = document.querySelector('#music51')
+let music52 = document.querySelector('#music52')
+let music53 = document.querySelector('#music53')
+let music54 = document.querySelector('#music54')
+let music55 = document.querySelector('#music55')
+let music56 = document.querySelector('#music56')
+let music57 = document.querySelector('#music57')
+let music58 = document.querySelector('#music58')
+let music59 = document.querySelector('#music59')
+let music60 = document.querySelector('#music60')
+let music61 = document.querySelector('#music61')
+let music62 = document.querySelector('#music62')
+let music63 = document.querySelector('#music63')
+let music64 = document.querySelector('#music64')
+let music65 = document.querySelector('#music65')
+let music66 = document.querySelector('#music66')
+
+let audios = document.getElementsByTagName('audio')
+
+function pauseAll(){
+    // audios[0].classList.forEach(function(audio,index){
+    //     audio.pause()
+    //     console(audio,index)
+    // });
+    for( let i = 0;i<66;i++){
+        audios[i].pause()
+    }
+}
+
+
+window.onload = function(){
+    console.log('audios',audios)
+    console.log(typeof(audios))
+    pauseAll()
+}
+    
+function dot1(){
+    if(music1.paused){
+        pauseAll()
+        music1.play();
+    }else{
+        music1.pause();
+    }
+}
+function dot2(){
+    if(music2.paused){
+        pauseAll()
+        music2.play();
+    }else{
+        music2.pause();
+    }
+}
+function dot3(){
+    if(music3.paused){
+        pauseAll()
+        music3.play();
+    }else{
+        music3.pause();
+    }
+}
+function dot4(){
+    if(music4.paused){
+        pauseAll()
+        music4.play();
+    }else{
+        music4.pause();
+    }
+}
+function dot5(){
+    if(music5.paused){
+        pauseAll()
+        music5.play();
+    }else{
+        music5.pause();
+    }
+}
+function dot6(){
+    if(music6.paused){
+        pauseAll()
+        music6.play();
+    }else{
+        music6.pause();
+    }
+}
+function dot7(){
+    if(music7.paused){
+        pauseAll()
+        music7.play();
+    }else{
+        music7.pause();
+    }
+}
+function dot8(){
+    if(music8.paused){
+        pauseAll()
+        music8.play();
+    }else{
+        music8.pause();
+    }
+}
+function dot9(){
+    if(music9.paused){
+        pauseAll()
+        music9.play();
+    }else{
+        music9.pause();
+    }
+}
+function dot10(){
+    if(music10.paused){
+        pauseAll()
+        music10.play();
+    }else{
+        music10.pause();
+    }
+}
+function dot11(){
+    if(music11.paused){
+        pauseAll()
+        music11.play();
+    }else{
+        music11.pause();
+    }
+}
+function dot12(){
+    if(music12.paused){
+        pauseAll()
+        music12.play();
+    }else{
+        music12.pause();
+    }
+}
+function dot13(){
+    if(music13.paused){
+        pauseAll()
+        music13.play();
+    }else{
+        music13.pause();
+    }
+}
+function dot14(){
+    if(music14.paused){
+        pauseAll()
+        music14.play();
+    }else{
+        music14.pause();
+    }
+}
+function dot15(){
+    if(music15.paused){
+        pauseAll()
+        music15.play();
+    }else{
+        music15.pause();
+    }
+}
+function dot16(){
+    if(music16.paused){
+        pauseAll()
+        music16.play();
+    }else{
+        music16.pause();
+    }
+}
+function dot17(){
+    if(music17.paused){
+        pauseAll()
+        music17.play();
+    }else{
+        music17.pause();
+    }
+}
+function dot18(){
+    if(music18.paused){
+        pauseAll()
+        music18.play();
+    }else{
+        music18.pause();
+    }
+}
+function dot19(){
+    if(music19.paused){
+        pauseAll()
+        music19.play();
+    }else{
+        music19.pause();
+    }
+}
+function dot20(){
+    if(music20.paused){
+        pauseAll()
+        music20.play();
+    }else{
+        music20.pause();
+    }
+}
+function dot21(){
+    if(music21.paused){
+        pauseAll()
+        music21.play();
+    }else{
+        music21.pause();
+    }
+}
+function dot22(){
+    if(music22.paused){
+        pauseAll()
+        music22.play();
+    }else{
+        music22.pause();
+    }
+}
+function dot23(){
+    if(music23.paused){
+        pauseAll()
+        music23.play();
+    }else{
+        music23.pause();
+    }
+}
+function dot24(){
+    if(music24.paused){
+        pauseAll()
+        music24.play();
+    }else{
+        music24.pause();
+    }
+}
+function dot25(){
+    if(music25.paused){
+        pauseAll()
+        music25.play();
+    }else{
+        music25.pause();
+    }
+}
+function dot26(){
+    if(music26.paused){
+        pauseAll()
+        music26.play();
+    }else{
+        music26.pause();
+    }
+}
+function dot27(){
+    if(music27.paused){
+        pauseAll()
+        music27.play();
+    }else{
+        music27.pause();
+    }
+}
+function dot28(){
+    if(music28.paused){
+        pauseAll()
+        music28.play();
+    }else{
+        music28.pause();
+    }
+}
+function dot29(){
+    if(music29.paused){
+        pauseAll()
+        music29.play();
+    }else{
+        music29.pause();
+    }
+}
+function dot30(){
+    if(music30.paused){
+        pauseAll()
+        music30.play();
+    }else{
+        music30.pause();
+    }
+}
+function dot31(){
+    if(music31.paused){
+        pauseAll()
+        music31.play();
+    }else{
+        music31.pause();
+    }
+}
+function dot32(){
+    if(music32.paused){
+        pauseAll()
+        music32.play();
+    }else{
+        music32.pause();
+    }
+}
+function dot33(){
+    if(music33.paused){
+        pauseAll()
+        music33.play();
+    }else{
+        music33.pause();
+    }
+}
+function dot34(){
+    if(music34.paused){
+        pauseAll()
+        music34.play();
+    }else{
+        music34.pause();
+    }
+}
+function dot35(){
+    if(music35.paused){
+        pauseAll()
+        music35.play();
+    }else{
+        music35.pause();
+    }
+}
+function dot36(){
+    if(music36.paused){
+        pauseAll()
+        music36.play();
+    }else{
+        music36.pause();
+    }
+}
+function dot37(){
+    if(music37.paused){
+        pauseAll()
+        music37.play();
+    }else{
+        music37.pause();
+    }
+}
+function dot38(){
+    if(music38.paused){
+        pauseAll()
+        music38.play();
+    }else{
+        music38.pause();
+    }
+}
+function dot39(){
+    if(music39.paused){
+        pauseAll()
+        music39.play();
+    }else{
+        music39.pause();
+    }
+}
+function dot40(){
+    if(music40.paused){
+        pauseAll()
+        music40.play();
+    }else{
+        music40.pause();
+    }
+}
+function dot41(){
+    if(music41.paused){
+        pauseAll()
+        music41.play();
+    }else{
+        music41.pause();
+    }
+}
+function dot42(){
+    if(music42.paused){
+        pauseAll()
+        music42.play();
+    }else{
+        music42.pause();
+    }
+}
+function dot43(){
+    if(music43.paused){
+        pauseAll()
+        music43.play();
+    }else{
+        music43.pause();
+    }
+}
+function dot44(){
+    if(music44.paused){
+        pauseAll()
+        music44.play();
+    }else{
+        music44.pause();
+    }
+}
+function dot45(){
+    if(music45.paused){
+        pauseAll()
+        music45.play();
+    }else{
+        music45.pause();
+    }
+}
+function dot46(){
+    if(music46.paused){
+        pauseAll()
+        music46.play();
+    }else{
+        music46.pause();
+    }
+}
+function dot47(){
+    if(music47.paused){
+        pauseAll()
+        music47.play();
+    }else{
+        music47.pause();
+    }
+}
+function dot48(){
+    if(music48.paused){
+        pauseAll()
+        music48.play();
+    }else{
+        music48.pause();
+    }
+}
+function dot49(){
+    if(music49.paused){
+        pauseAll()
+        music49.play();
+    }else{
+        music49.pause();
+    }
+}
+function dot50(){
+    if(music50.paused){
+        pauseAll()
+        music50.play();
+    }else{
+        music50.pause();
+    }
+}
+function dot51(){
+    if(music51.paused){
+        pauseAll()
+        music51.play();
+    }else{
+        music51.pause();
+    }
+}
+function dot52(){
+    if(music52.paused){
+        pauseAll()
+        music52.play();
+    }else{
+        music52.pause();
+    }
+}
+function dot53(){
+    if(music53.paused){
+        pauseAll()
+        music53.play();
+    }else{
+        music53.pause();
+    }
+}
+function dot54(){
+    if(music54.paused){
+        pauseAll()
+        music54.play();
+    }else{
+        music54.pause();
+    }
+}
+function dot55(){
+    if(music55.paused){
+        pauseAll()
+        music55.play();
+    }else{
+        music55.pause();
+    }
+}
+function dot56(){
+    if(music56.paused){
+        pauseAll()
+        music56.play();
+    }else{
+        music56.pause();
+    }
+}
+function dot57(){
+    if(music57.paused){
+        pauseAll()
+        music57.play();
+    }else{
+        music57.pause();
+    }
+}
+function dot58(){
+    if(music58.paused){
+        pauseAll()
+        music58.play();
+    }else{
+        music58.pause();
+    }
+}
+function dot59(){
+    if(music59.paused){
+        pauseAll()
+        music59.play();
+    }else{
+        music59.pause();
+    }
+}
+function dot60(){
+    if(music60.paused){
+        pauseAll()
+        music60.play();
+    }else{
+        music60.pause();
+    }
+}
+function dot61(){
+    if(music61.paused){
+        pauseAll()
+        music61.play();
+    }else{
+        music61.pause();
+    }
+}
+function dot62(){
+    if(music62.paused){
+        pauseAll()
+        music62.play();
+    }else{
+        music62.pause();
+    }
+}
+function dot63(){
+    if(music63.paused){
+        pauseAll()
+        music63.play();
+    }else{
+        music63.pause();
+    }
+}
+function dot64(){
+    if(music64.paused){
+        pauseAll()
+        music64.play();
+    }else{
+        music64.pause();
+    }
+}
+function dot65(){
+    if(music65.paused){
+        pauseAll()
+        music65.play();
+    }else{
+        music65.pause();
+    }
+}
+function dot66(){
+    if(music66.paused){
+        pauseAll()
+        music66.play();
+    }else{
+        music66.pause();
+    }
+}
+
